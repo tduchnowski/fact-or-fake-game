a35bd87c0796683b35f1c5917d401a09a27ff7a6
Tomasz Duchnowski <entidzow@gmail.com>
2025-09-04 21:47:06 +0400

    Added tests for PlayerInfo model

--- TrueFalseBackend.Tests/Unit/Models.Tests/PlayerTests.cs
+++ TrueFalseBackend.Tests/Unit/Models.Tests/PlayerTests.cs
@@ -1,50 +1,140 @@
+using TrueFalseBackend.Models;
+
 public class PlayerTests
 {
     [Fact]
-    public void TestPlayerInfo_AddPlayer_CorrectCount()
+    public void TestPlayerInfo_AddPlayerToEmpty_CorrectCount()
     {
-
+        PlayersInfo pi = new();
+        pi.AddPlayer("player");
+        Assert.Equal(1, pi.Count());
     }
 
     [Fact]
     public void TestPlayersInfo_AddPlayer_PlayerIsHost()
     {
+        PlayersInfo pi = new();
+        pi.AddPlayer("player");
+        Assert.True(pi.IsPlayerHost("player"));
+    }
 
+    [Theory]
+    [InlineData(100)]
+    [InlineData(200)]
+    [InlineData(300)]
+    [InlineData(1234)]
+    [InlineData(2345)]
+    public void TestPlayersInfo_NotExistingPlayerIsHost_ReturnFalse(int input)
+    {
+        PlayersInfo pi = new();
+        for (int i = 0; i < input; i++)
+        {
+            pi.AddPlayer($"player{i}");
+        }
+        Assert.False(pi.IsPlayerHost("p"));
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(100)]
+    [InlineData(200)]
+    [InlineData(300)]
+    [InlineData(1234)]
+    [InlineData(2345)]
+    public void TestPlayersInfo_AddMultiplePlayers_CorrectCount(int input)
+    {
+        PlayersInfo pi = new();
+        for (int i = 0; i < input; i++)
+        {
+            pi.AddPlayer($"player{i}");
+        }
+        Assert.Equal(input, pi.Count());
+    }
+
+    [Theory]
+    [InlineData(100)]
+    [InlineData(200)]
+    [InlineData(300)]
+    [InlineData(1234)]
+    [InlineData(2345)]
+    public void TestPlayerInfo_AddMultiplePlayers_OnlyFirstPlayerIsHost(int input)
+    {
+        PlayersInfo pi = new();
+        string playerHost = "host";
+        pi.AddPlayer(playerHost);
+        List<string> playerIds = [];
+        for (int i = 1; i < input; i++)
+        {
+            string playerId = $"player{i}";
+            pi.AddPlayer(playerId);
+            playerIds.Add(playerId);
+        }
+        Assert.True(pi.IsPlayerHost(playerHost));
+        Assert.DoesNotContain(playerIds, id => pi.IsPlayerHost(id));
+    }
+
+    [Theory]
+    [InlineData(100, 50)]
+    [InlineData(200, 101)]
+    [InlineData(300, 299)]
+    [InlineData(1234, 1000)]
+    [InlineData(2345, 1999)]
+    public void TestPlayerInfo_RemovePlayer_PlayerNotInPlayersDictionary(int numberOfPlayers, int idToRemove)
+    {
+        PlayersInfo pi = new();
+        string toRemove = $"player{idToRemove}";
+        for (int i = 1; i < numberOfPlayers; i++)
+        {
+            string playerId = $"player{i}";
+            pi.AddPlayer(playerId);
+        }
+        pi.RemovePlayer(toRemove);
+        Assert.Null(pi.GetPlayer(toRemove));
+    }
+
+    [Theory]
+    [InlineData(0, 200)]
+    [InlineData(100, 200)]
+    [InlineData(200, 201)]
+    [InlineData(300, 576)]
+    [InlineData(1234, 28997)]
+    [InlineData(2345, 10082)]
+    public void TestPlayersInfo_RemoveNotPresentPlayer_DoNothing(int numberOfPlayers, int notExistingId)
+    {
+        PlayersInfo pi = new();
+        string toRemove = $"player{notExistingId}";
+        for (int i = 1; i <= numberOfPlayers; i++)
+        {
+            string playerId = $"player{i}";
+            pi.AddPlayer(playerId);
+        }
+        pi.RemovePlayer(toRemove);
+        Assert.Equal(numberOfPlayers, pi.Count());
     }
 
     [Fact]
-    public void TestPlayersInfo_NotExistingPlayerIsHost_ReturnFalse()
+    public void TestPlayersInfo_RemoveHost_AssignedNewHost()
     {
 
     }
 
-    [Fact]
-    public void TestPlayerInfo_AddMultiplePlayers_FirstPlayerIsHost()
+    [Theory]
+    [InlineData(100, 50)]
+    [InlineData(200, 101)]
+    [InlineData(300, 199)]
+    [InlineData(1234, 1000)]
+    [InlineData(2345, 100)]
+    public void TestPlayersInfo_AddAndRemovePlayers_CorrectCount(int numberOfPlayers, int numberToRemove)
     {
-
-    }
-
-    [Fact]
-    public void TestPlayersInfo_Count_EqualsToPlayersAdded()
-    {
-
-    }
-
-    [Fact]
-    public void TestPlayerInfo_RemovePlayer_PlayerNotInPlayersDictionary()
-    {
-
-    }
-
-    [Fact]
-    public void TestPlayersInfo_RemoveNotPresentPlayer_DoNothing()
-    {
-
-    }
-
-    [Fact]
-    public void TestPlayersInfo_AddAndRemovePlayers_CorrectCount()
-    {
-
+        PlayersInfo pi = new();
+        for (int i = 1; i <= numberOfPlayers; i++)
+        {
+            string playerId = $"player{i}";
+            pi.AddPlayer(playerId);
+        }
+        Random r = new();
+        List<int> randomIds = Enumerable.Range(1, numberOfPlayers).OrderBy(_ => r.Next()).Take(numberToRemove).ToList();
+        foreach (int id in randomIds) pi.RemovePlayer($"player{id}");
+        Assert.Equal(numberOfPlayers - numberToRemove, pi.Count());
     }
 }
--- TrueFalseBackend.Tests/Unit/Models.Tests/RoomStateTests.cs
+++ TrueFalseBackend.Tests/Unit/Models.Tests/RoomStateTests.cs
@@ -5,8 +5,8 @@ public class RoomStateTests
     [Fact]
     public void TestRound_Advancing_IncrementRoundId()
     {
-        Round round = new() { Id = 1, RoundQuestion = new() };
-        for (int i = 2; i < 100; i++)
+        Round round = new() { Id = 0, RoundQuestion = new() };
+        for (int i = 1; i < 100; i++)
         {
             Question nextQuestion = new() { Id = i, Answer = true, Text = $"Text{i}" };
             round.Next(nextQuestion);
