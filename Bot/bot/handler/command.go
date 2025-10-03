e8174e472d5540dae92837827a767cf376eed854
Tomasz Duchnowski <entidzow@gmail.com>
2025-10-04 01:05:35 +0400

    Updated passing user data to X-Telegram-InitData as a Telegram bot

--- Bot/bot/handler/command.go
+++ Bot/bot/handler/command.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 
 	"FactOrFake.Bot/telegramapi"
@@ -51,25 +54,31 @@ func (cmdHandler CommandHandler) GetResponder(msg *telegramapi.Message) Responde
 }
 
 func start(msg *telegramapi.Message) Responder {
-	text := "Hello and Welcome!\n\nThis is an game app that allows you to play True/False quiz games either by yourself or with friends.\n\nYou can also directly create game rooms through this bot. Whether you create a room with a bot or through a mini app, just share the link with your friends and start playing with them!\n\nClick Start to launch an app\n\nHave fun!"
+	text := "Hello and welcome!\n\nThis is a mini app that allows you to play True/False quiz games either by yourself or with friends.\n\nYou can also directly create game rooms through this bot. Whether you create a room with a bot or through a mini app, just share the link with your friends and start playing with them!\n\nClick Start to launch an app\n\nHave fun!"
 	return SendMsg{ChatId: msg.Chat.Id, Text: text}
 }
 
 func help(msg *telegramapi.Message) Responder {
-	text := "help"
+	text := "Creating a game:\n\n/createGame <number of rounds> <round duration in seconds>\n\nYou can also just open the mini app and play there"
 	return SendMsg{ChatId: msg.Chat.Id, Text: text}
 }
 
 func createGame(msg *telegramapi.Message, httpClient *http.Client, requestUrl, apiKey string) Responder {
-	roundsNum := "10"
-	roundTimeout := "10"
+	msgText := msg.Text
+	cmdParts := strings.Fields(msgText)
+	if len(cmdParts) != 3 {
+		return SendMsg{ChatId: msg.Chat.Id, Text: "Incorrect arguments for this command\n\nExample:\n/createGame 10 5\n\nwhere 10 is the number of rounds you want and 5 is the maximum round duration"}
+	}
+	roundsNum, roundTimeout := cmdParts[1], cmdParts[2]
 	createRoomUrl := fmt.Sprintf("%s/createRoom?roundsNum=%s&roundTimeout=%s", requestUrl, roundsNum, roundTimeout)
 	request, err := http.NewRequest("GET", createRoomUrl, nil)
 	if err != nil {
 		slog.Error(err.Error())
 		return SendMsg{ChatId: msg.Chat.Id, Text: "Internal server error. Please try again later"}
 	}
-	request.Header.Add("X-Telegram-InitData", "bot "+apiKey)
+	userInitData := createUserInitData(msg)
+	initDataHeader := fmt.Sprintf("bot %s %s", apiKey, userInitData)
+	request.Header.Add("X-Telegram-InitData", initDataHeader)
 	response, err := httpClient.Do(request)
 	if err != nil {
 		slog.Error(err.Error())
@@ -78,6 +87,7 @@ func createGame(msg *telegramapi.Message, httpClient *http.Client, requestUrl, a
 	defer response.Body.Close()
 	if response.StatusCode != 200 {
 		slog.Error(fmt.Sprintf("createRoomUrl: %s, status code: %d", createRoomUrl, response.StatusCode))
+		return SendMsg{ChatId: msg.Chat.Id, Text: "Couldn't create a room. Try again later"}
 	}
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
@@ -86,4 +96,15 @@ func createGame(msg *telegramapi.Message, httpClient *http.Client, requestUrl, a
 	}
 	fmt.Printf("%s\n", body)
 	return SendMsg{ChatId: msg.Chat.Id, Text: string(body)}
+}
+
+func createUserInitData(msg *telegramapi.Message) string {
+	userJsonString, err := json.Marshal(msg.From)
+	if err != nil {
+		slog.Error("marshallig of user init data failed: " + err.Error())
+		return ""
+	}
+	values := url.Values{}
+	values.Set("user", string(userJsonString))
+	return values.Encode()
 }
